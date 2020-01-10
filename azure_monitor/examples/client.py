432b25a9dc7d55bbdcd2413508dda286f07ee979
Leighton <lechen@microsoft.com>
2020-01-10 12:29:09 -0800

    test

--- azure_monitor/examples/client.py
+++ azure_monitor/examples/client.py
@@ -1,5 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
+# Licensed under the MIT License...
 import requests
 
 from azure_monitor import AzureMonitorSpanExporter
